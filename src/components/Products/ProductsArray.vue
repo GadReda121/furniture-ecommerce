f519aef72bf495c48914e462ac75e835d8397382
GadReda121 <khtabjab@gmail.com>
2023-08-13 17:41:37 +0300

    Ready To Do Card + Desc

---
+++ src/components/Products/ProductsArray.vue
@@ -0,0 +1,140 @@
+<template>
+      <div class="item-list">
+        <div class="item" v-for="(item, index) in items" :key="index">
+          <div class="item-image-container">
+            <img :src="item.image" alt="Item Image" class="item-image">
+            <div class="item-buttons">
+              <button class="add-to-cart" @click="addToCart(item)">Add to Cart</button>
+              <button class="item-description" @click="showDescription(item)">Description</button>
+            </div>
+          </div>
+          <h3 class="item-title">{{ item.title }}</h3>
+          <p class="item-price">{{ formatCurrency(item.price) }}</p>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    name:"ProductsArray",
+    data() {
+        return {
+            items: [
+                {
+                    id:'1',
+                    image: '../../../public/images/Products/1.jpg',
+                    title: 'Item 1',
+                    price: 50.00,
+                },
+                {
+                    id:'2',
+                    image: '../../../public/images/Products/2.jpg',
+                    title: 'Item 2',
+                    price: 75.00,
+                },
+                {
+                    id:'3',
+                    image: '../../../public/images/Products/3.jpg',
+                    title: 'Item 3',
+                    price: 80.00,
+                },
+                {
+                    id:'4',
+                    image: '../../../public/images/Products/4.jpg',
+                    title: 'Item 4',
+                    price: 90.00,
+                },
+                {
+                    id:'5',
+                    image: '../../../public/images/Products/5.jpg',
+                    title: 'Item 5',
+                    price: 100.00,
+                },
+                {
+                    id:'6',
+                    image: '../../../public/images/Products/2.jpg',
+                    title: 'Item 6',
+                    price: 120.00,
+                },
+                // ... other items
+            ],
+        };
+    },
+    methods: {
+        formatCurrency(value) {
+            return `$${value.toFixed(2)}`;
+        },
+        addToCart(item) {
+            // Implement your logic to add the item to the cart
+            console.log('Added to cart:', item);
+        },
+        showDescription(item) {
+            // Implement your logic to show the item's description
+            console.log('Description:', item.title);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.item-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+}
+
+.item {
+    width: 250px;
+    border: 1px solid #ccc;
+    padding: 10px;
+    text-align: center;
+    background-color: #fff;
+    box-shadow: 0 0 10px rgba(0,0,0,.1);
+}
+
+.item-image-container {
+    position: relative;
+    overflow: hidden;
+}
+
+.item-image {
+    width: 100%;
+    height: auto;
+    transition: transform 0.3s;
+}
+
+.item-image-container:hover .item-image {
+    transform: scale(1.1);
+    /* Enlarge the image on hover */
+}
+
+.item-buttons {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(255, 255, 255, 0.9);
+    padding: 5px;
+    display: none;
+    background-color: rgba(9, 65, 176, 0.669);
+    height: 100%;
+    align-items: center;
+    margin: auto;
+}
+.item-buttons button{
+    border-radius: 5px;
+    background:none;
+    border: 1px solid #fff;
+    color: #ffffff;
+    padding: 10px;
+    width: 50%;
+    margin: auto;
+}
+.item-image-container:hover .item-buttons {
+    display: grid;
+    align-content: center;
+    grid-gap: .5rem;
+    /* Show buttons on hover */
+}
+
+/* Other styles for item title and price */</style>
--- src/components/Products/ProductsItems.vue
+++ src/components/Products/ProductsItems.vue
@@ -15,16 +15,69 @@
         <!-- All Products -->
         <div class="allProducts">
             <!-- sidebar -> Fiter -->
-            <div class="sideBarFilter"></div>
+            <div class="sideBarFilter">
+                <div class="category">
+                    <h5>Categories +</h5>
+                    <ul>
+                        <li>
+                            <RouterLink to="">
+                                Table
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="">
+                                Lamps
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="">
+                                Chairs
+                            </RouterLink>
+                        </li>
+                        <li>
+                            <RouterLink to="">
+                                Sofas
+                            </RouterLink>
+                        </li>
+                    </ul>
+                </div>
+                <div class="color mt-5">
+                    <h5>Filter By + </h5>
+                    <div class="colors d-flex flex-column">
+                        <h6>Color</h6>
+                        <dic class="cos">
+                            <div class="co"></div>
+                            <div class="co"></div>
+                            <div class="co"></div>
+                            <div class="co"></div>
+                        </dic>
+                    </div>
+                </div>
+                <div class="range mt-5">
+                    <h5>Price Range</h5>
+                    <input type="range" v-model="selectedValue" min="0" max="5000" step="10" />
+                    {{ selectedValue + '$' }}
+                </div>
+            </div>
             <!-- Products -> Content -->
-            <div class="ProductsContent"></div>
+            <div class="ProductsContent">
+                <ProductsArray />
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import ProductsArray from './ProductsArray.vue';
+
 export default {
     name: "ProductItems",
+    data() {
+        return {
+            selectedValue: 0,
+        };
+    },
+    components: { ProductsArray }
 }
 </script>
 
@@ -46,7 +99,7 @@ export default {
 }
 .ProductItems .allProducts{
     display: grid;
-    grid-template-columns: 30% 65%;
+    grid-template-columns: 25% 75%;
     grid-gap: 1rem;
     margin-bottom: 2rem;
 }
@@ -54,6 +107,54 @@ export default {
     background-color: #2C3539;
     padding: 30px;
     color: #ffffff;
-    box-shadow: 0 0 30px rgba(0,0,0,.2);
+    height: 90vh;
+    box-shadow: 0 0 50px rgba(0,0,0,.3);
+}
+ul{
+    list-style: none;
+    padding: 10px;
+}
+ul>li{
+    margin-top: .5rem;
+}
+ul>li>a{
+    text-decoration: none;
+    color: #ffffff;
+}
+.ProductItems .allProducts .sideBarFilter .color .colors{
+    display: flex;
+    gap: .5rem;
+}
+.cos{
+    display: flex;
+    gap:.5rem
+}
+.co{
+    border-radius: 50%;
+    height: 15px;
+    width: 15px;
+}
+.co:nth-child(1){
+    background-color: yellow;
+}
+.co:nth-child(2){
+    background-color: blue;
+}
+.co:nth-child(3){
+    background-color: white;
+}
+.co:nth-child(4){
+    background-color: red;
+}
+
+@media screen and (max-width: 767px){
+    .ProductItems .allProducts{
+        grid-template-columns: 50% 50%;
+    }
+}
+@media screen and (min-width: 768px) and (max-width:991px){
+    .ProductItems .allProducts{
+        grid-template-columns: 40% 60%;
+    }
 }
 </style>
